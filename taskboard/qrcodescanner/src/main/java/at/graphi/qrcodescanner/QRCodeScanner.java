6276f3939f8e5c3467456aeba18ba67e629a2edc
SGrasmann <grasi_sg@yahoo.de>
2018-07-19 15:11:24 +0200

    QRScanner Version ChristanGrasiTorsten

--- taskboard/qrcodescanner/src/main/java/at/graphi/qrcodescanner/QRCodeScanner.java
+++ taskboard/qrcodescanner/src/main/java/at/graphi/qrcodescanner/QRCodeScanner.java
@@ -1,18 +1,15 @@
 package at.graphi.qrcodescanner;
 
-import java.awt.image.BufferedImage;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import com.google.zxing.BinaryBitmap;
-import com.google.zxing.EncodeHintType;
+import com.google.zxing.NotFoundException;
 import com.google.zxing.Result;
+import com.google.zxing.ResultPoint;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.common.HybridBinarizer;
 import com.google.zxing.multi.qrcode.QRCodeMultiReader;
-import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 import at.graphi.interfaces.CodeScanner;
 import at.graphi.model.Bild;
@@ -22,51 +19,49 @@ import at.graphi.model.Taskboard;
 
 public class QRCodeScanner implements CodeScanner {
 
-	private static final String CHARSET = "UTF-8"; // or "ISO-8859-1"
-	private Map<EncodeHintType, ErrorCorrectionLevel> hintMap;
-
-	public QRCodeScanner() {
-		this.hintMap = generateHintMap();
-	}
-
-	private Map<EncodeHintType, ErrorCorrectionLevel> generateHintMap() {
-		Map<EncodeHintType, ErrorCorrectionLevel> hintMap = new HashMap<EncodeHintType, ErrorCorrectionLevel>();
-		hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
-		return hintMap;
-	}
-
 	@Override
 	public Taskboard generateTaskboard(Bild bild, List<Spalte> spalten) {
-		List<Result> listOfQRCodesInImage = Arrays.asList(estimateQRCodes(bild, CHARSET, hintMap));
-		for (Result result : listOfQRCodesInImage) {
-			float middleX = calculateMiddleX(result);
-			for (Spalte spalte : spalten) {
-				if (middleX < spalte.getLeftX()) {
-					spalte.addEpic(new Epic(result.getText()));
-					break;
-				}
-			}
+		Taskboard board = new Taskboard();
+		board.setSpalten(spalten);
+
+		BinaryBitmap binaryBitmap = new BinaryBitmap(
+				new HybridBinarizer(new BufferedImageLuminanceSource(bild.getImage())));
+
+		Result[] qrCodeResult = readQRCodesFromImage(binaryBitmap);
+		List<Result> results = Arrays.asList(qrCodeResult);
+
+		for (Result result : results) {
+			ResultPoint[] resultPoints = result.getResultPoints();
+
+			addEpicToSpalte(spalten, result, calculateMeanPosition(resultPoints));
 		}
-		return new Taskboard(spalten);
+
+		board.setSpalten(spalten);
+
+		return board;
 	}
 
-	private float calculateMiddleX(Result result) {
-		float middleX = result.getResultPoints()[0].getX();
-
-		for (int i = 1; i < result.getResultPoints().length; i++) {
-			middleX = (middleX + result.getResultPoints()[i].getX()) / 2;
-		}
-		return middleX;
-	}
-
-	private Result[] estimateQRCodes(Bild bild, String charset, Map hintMap) {
+	private Result[] readQRCodesFromImage(BinaryBitmap binaryBitmap) {
 		try {
-			BufferedImage image = bild.getImage();
-			BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(new BufferedImageLuminanceSource(image)));
-			QRCodeMultiReader multiReader = new QRCodeMultiReader();
-			return multiReader.decodeMultiple(binaryBitmap, hintMap);
-		} catch (Exception e) {
-			return null;
+			return new QRCodeMultiReader().decodeMultiple(binaryBitmap);
+		} catch (NotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private float calculateMeanPosition(ResultPoint[] resultPoints) {
+		return (resultPoints[2].getX() + resultPoints[1].getX()) / 2;
+	}
+
+	private void addEpicToSpalte(List<Spalte> spalten, Result result, float position) {
+		for (Spalte spalte : spalten) {
+
+			if (spalte.getLeftX() > position) {
+				Epic epic = new Epic(result.getText());
+				spalte.addEpic(epic);
+				break;
+			}
 		}
 	}
 
